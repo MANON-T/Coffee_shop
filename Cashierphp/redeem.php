7040705852980d2c536b063af8a060e2d926ef7c
SCLERA <manon301260@gmail.com>
2024-03-16 14:23:01 +0700

    ปรับปรุงการเรียกใช้ไฟล์ css
    - แยกไฟล์ css ต่มหมวดหมู่ให้ถูกต้อง และเรียกใช้แค่ไฟล์ css ที่ตัวเองควรใช้

--- Cashierphp/redeem.php
+++ Cashierphp/redeem.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['cashier_login'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../css/add_styles.css">
+    <link rel="stylesheet" href="../css/cashier_styles.css">
     <title>Order Checkout</title>
 </head>
 
--- Managerphp/dessert_config.php
+++ Managerphp/dessert_config.php
@@ -35,7 +35,7 @@ if (!empty($_GET['id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../css/add_styles.css">
+    <link rel="stylesheet" href="../css/manager_styles.css">
     <title>Manage Product</title>
     <style>
         .form-container {
