07f5b2ed6286cd9e8a64d93fa89b87a474391bb8
SCLERA <manon301260@gmail.com>
2024-03-16 14:36:15 +0700

    ลบไฟล์ที่ไม่ได้ใช้
    -ลบไฟล์ที่ไม่ได้ใช้
    -แก้ไข part ที่ยังไม่ถูกต้อง

--- Userphp/Information.php
+++ Userphp/Information.php
@@ -46,7 +46,7 @@ if (isset($_POST['delete'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/add_styles.css">
+    <link rel="stylesheet" href="../css/user_styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Profile</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
